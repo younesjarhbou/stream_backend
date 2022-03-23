4a542b9254af94e77784942c245dc402d804bea3
Younes Jarhbou <81886160+younesjarhbou@users.noreply.github.com>
2022-03-23 04:56:17 +0100

    set production serverpath

--- util/serverPath.js
+++ util/serverPath.js
@@ -1,5 +1,5 @@
 
-exports.serverPath = "http://172.26.52.87:5000/"; // example: https://example.example.com/
+exports.serverPath = "https://6239512612b34c33683a95c8--fascinating-semolina-9325fa.netlify.app/"; // example: https://example.example.com/
 
 
 exports.serverKey = "AAAAnSn30-Y:APA91bGo5ABfPMF11cG4VBLrn1pqhIkieWoI8AxiapibJinS3AXib2y_qeBD_KjB-37LKscYxFR8zDVm3AlBqRFGJCRBHeJ2yzEjFwcCB-UKxSR4o7VLL0WpAnTx6qctgVG3A1JQrccj"; //get this from firebase
